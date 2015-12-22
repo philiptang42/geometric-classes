84351779cbd1fe4e38b82a70769be8509a3b41b6
philiptang42 <philiptang42@gmail.com>
2015-12-21 23:00:32 -0500

    area of a circle

--- lib/circle.rb
+++ lib/circle.rb
@@ -1,6 +1,8 @@
 class Circle
   attr_reader :radius, :x, :y
 
+  Pi = 3.14159265358979
+
   def initialize(radius, x = 0, y = 0)
     @radius = radius
     @x = x
@@ -10,5 +12,9 @@ class Circle
   def diameter
     @radius * 2
   end
-  
+
+  def area
+    (@radius ** 2) * Pi
+  end
+
 end
