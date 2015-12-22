e4a76e7067bb3794863b570775edf53c253acaad
philiptang42 <philiptang42@gmail.com>
2015-12-21 22:05:14 -0500

    7 circle tests passing

--- lib/circle.rb
+++ lib/circle.rb
@@ -1,8 +1,14 @@
 class Circle
   attr_reader :radius, :x, :y
+
   def initialize(radius, x = 0, y = 0)
     @radius = radius
     @x = x
     @y = y
   end
+
+  def diameter
+    @radius * 2
+  end
+  
 end
